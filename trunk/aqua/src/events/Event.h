daeb04f22a8138ff99891834b30708119033c7fb
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-02-01 17:33:39 +0000

    Completed the EventFactory, Created a basic event SimpleEvent, and finished the serialization implementation for Event.

--- trunk/aqua/src/events/Event.h
+++ trunk/aqua/src/events/Event.h
@@ -21,7 +21,10 @@
 #define EVENT_TYPE_HOVER 3
 #define EVENT_TYPE_OTHER 4
 
+#include <stdio.h> // TODO remove
 #include <string>
+
+#include "../utils/EndianConverter.h"
 
 class Event {
 
@@ -35,12 +38,84 @@ private:
 
 // Methods
 public:
-    Event(char* eventData);
-    virtual ~Event();
+    Event(char* eventData) {
+        int i;
+        
+        // Get the name - this should read up to a null character.
+        _name = eventData;
+        eventData += (_name.length() + 1);
+        
+        // Get the description - this should behave similarly.
+        _description = eventData;
+        eventData += (_name.length() + 1);
+        
+        // Get the type.
+        _type = *eventData;
+        eventData++;
+        
+        // Get the ID.
+        memcpy(&_id, eventData, 4);
+        eventData++;
     
+        // Get the location.
+        memcpy(_location, eventData, 12);
+        
+        // Swap endianness if needed.
+        if (EndianConverter::isLittleEndian()) {
+            
+            _id = EndianConverter::swapIntEndian(_id);
+            for(i = 0; i < 3; i++) {
+                _location[i] = EndianConverter::swapFloatEndian(_location[i]);
+            }
+        }
+    }
+    
+    virtual ~Event() {}
+    
+    /**
+     * Serializes this event's data.
+     */
     char* serialize() {
-        // TODO implement this method
-        return NULL;
+        int i;
+        int myLength = (_name.length() + _description.length() + 2) + 17;
+        
+        int   subclassLength = 0;
+        char* subclassData = serializeData(subclassLength);
+        
+        char* ret = new char[myLength + subclassLength];
+        char* bufferPtr = ret;
+        
+        // Name
+        memcpy(bufferPtr, _name.c_str(), _name.length() + 1);
+        bufferPtr += _name.length() + 1;
+        
+        // Description
+        memcpy(bufferPtr, _description.c_str(), _description.length() + 1);
+        bufferPtr += _description.length() + 1;
+        
+        // Type
+        *bufferPtr = _type;
+        bufferPtr++;
+        
+        // ID
+        memcpy(bufferPtr, &_id, 4);
+        if (EndianConverter::isLittleEndian()) {
+            *bufferPtr = EndianConverter::swapIntEndian(*bufferPtr);
+        }
+        bufferPtr += 4;
+        
+        // Location
+        for (i = 0; i < 3; i++) {
+            memcpy(bufferPtr, &(_location[i]), 4);
+            *bufferPtr = EndianConverter::swapFloatEndian(*bufferPtr);
+            bufferPtr += 4;
+        }
+        
+        // Subclass data
+        memcpy(bufferPtr, subclassData, subclassLength);
+        
+        delete subclassData;
+        return ret;
     };
     
     void  setID(int id) {
@@ -51,7 +126,7 @@ public:
     int  getID()   { return _id;   };
     
 private:
-    virtual char* serializeData(int* outLength) = 0;
+    virtual char* serializeData(int &lengthOut) = 0;
     
 };
 
