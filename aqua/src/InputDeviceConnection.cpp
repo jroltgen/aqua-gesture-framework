8035cb713959c497d238abfbdaa18912561b7491
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-02-01 23:47:51 +0000

    Fixing bugs in InputDeviceConnection; adding better error reporting in EventFactory & GestureFactory; Fixing bugs in Event

--- aqua/src/InputDeviceConnection.cpp
+++ aqua/src/InputDeviceConnection.cpp
@@ -18,6 +18,38 @@
 
 using namespace std;
 
+//////////////// TODO remove this/
+/*
+string getType(char type) {
+    switch(type) {
+    case EVENT_TYPE_DOWN:
+        return string("DOWN");
+    case EVENT_TYPE_MOVE:
+        return string("MOVE");
+    case EVENT_TYPE_UP:
+        return string("UP");
+    case EVENT_TYPE_HOVER:
+        return string("OTHER");
+    case EVENT_TYPE_OTHER:
+        return string("HOVER");
+    }
+    return string("UNRECOGNIZED");
+}
+    
+    
+void printEvent(Event* event) {
+    printf("[PrintEventGesture] Received Event.\n");
+    printf("\t%s : %s\n", event->getName().c_str(), event->getDesc().c_str());
+    printf("\tType: %s ---", getType(event->getType()).c_str());
+    printf(" ID: %d\n", event->getID());
+    printf("\tX: %f\n\tY: %f\n\tZ: %f\n", event->getX(), event->getY(), 
+            event->getZ());
+}
+*/
+////////////////////////////////////
+
+using namespace std;
+
 #ifdef _WIN32
 InputDeviceConnection::InputDeviceConnection(SOCKET theSocket, 
         GestureServer* theServer, int id) {
@@ -48,48 +80,65 @@ void InputDeviceConnection::run() {
  * Reads events from the input stream.
  */
 void InputDeviceConnection::readEvents() {
-    printf("[InputDeviceConnection] Connected, receiving events.\n");
-    while (true) {
-        readEvent();
-    }
-}
-
-/**
- * Handle a socket error
- */
-void InputDeviceConnection::handleError(int sockError) {
-    printf("Socket error, bytes received: %d\n", sockError);
-    // TODO add better error handling - we need to close this connection.
+    printf("[InputDeviceConnection] ID(%d) Connected\n", _id);
+    bool ok;
+    do {
+        ok = readEvent();
+    } while (ok);
+    
+    // Delete myself, no one has a reference to me.
+    printf("[InputDeviceConnection] ID(%d) Disconnected\n", _id);
+    delete this;
 }
 
 /**
  * Read a single event from the input stream.
  */
 #ifdef _WIN32
-void InputDeviceConnection::readEvent() {
+bool InputDeviceConnection::readEvent() {
     int iResult;
     unsigned short msgLength;
+    int remaining;
     // TODO implement 
     // Read event length.
     iResult = recv(_socket, receiveBuffer, 2, 0);
-    if (iResult != 1) {
-        handleError(iResult);
+    if (iResult != 2) {
+        printf("Error::: %ld\n", WSAGetLastError());
+        return false;
     }
+    printf("Received length\n");
     
-    msgLength = *receiveBuffer;
+    memcpy(&msgLength, receiveBuffer, 2);
+    //msgLength = *receiveBuffer;
+
     if (EndianConverter::isLittleEndian()) {
         msgLength = EndianConverter::swapShortEndian(msgLength);
+        printf("is little endian.\n");
     }
-    printf("Message length: %d\n", msgLength);
+    printf("Message length: %hd\n", msgLength);
     
-    iResult = recv(_socket, receiveBuffer, msgLength, 0);
-    
+    remaining = msgLength;
+    char* bufferPtr = receiveBuffer;
+    while (remaining > 0) {
+        iResult = recv(_socket, bufferPtr, remaining, 0);
+        remaining -= iResult;
+        printf("Data received...length: %d\n", iResult);
+        bufferPtr += iResult;
+    }
+       
     Event* receivedEvent = EventFactory::getInstance()->createEvent(
             string(receiveBuffer), receiveBuffer);
+            
+    // Add our constant to the eventID to avoid conflicting event IDs.
+    receivedEvent->setID(_id * 100000 + receivedEvent->getID());
     
+    //printf("Created event...\n");
+    //printEvent(receivedEvent);
+      
     _server->processEvent(receivedEvent);
     
     delete receivedEvent;
+    return true;
 }
 #else
 // TODO linux support
