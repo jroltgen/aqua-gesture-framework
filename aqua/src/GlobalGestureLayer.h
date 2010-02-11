134797b0059499675eac6361cca2b3fa94352f26
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-02-11 14:06:51 +0000

    Updated to follow the LGPL guidelines provided by http://www.gnu.org/licenses/gpl-howto.html

--- aqua/src/GlobalGestureLayer.h
+++ aqua/src/GlobalGestureLayer.h
@@ -6,10 +6,21 @@
  * they only publish to the client - they are not allowed to publish back
  * to themselves like the translaters in the GestureEngine are.
  * 
- * Written as part of the Aqua Universal Gesture Recognition Framework.
- * Licensed under the LGPL.
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- * Jay Roltgen, 2010
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _GLOBALGESTURELAYER_H_
 #define _GLOBALGESTURELAYER_H_
--- aqua/src/gestures/YouRockGesture.h
+++ aqua/src/gestures/YouRockGesture.h
@@ -3,10 +3,21 @@
  * 
  * Prints "Congratulations, you're awesome." when an event is received.
  * 
- * Written as part of the Aqua Universal Gesture Recognition Framework.
- * Licensed under the LGPL.
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- * Jay Roltgen, 2010
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _YOUROCKGESTURE_H_
 #define _YOUROCKGESTURE_H_
