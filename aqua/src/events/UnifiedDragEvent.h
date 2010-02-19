6b2873f95556af20b15bcf2680b1b151d81a5b38
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-02-19 20:32:09 +0000

    Added UnifiedDragEvent

---
+++ aqua/src/events/UnifiedDragEvent.h
@@ -0,0 +1,50 @@
+/**
+ * UnifiedDragEvent.h
+ *
+ * An event to use for sending drag-related information.
+ * 
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _UNIFIEDDRAGEVENT_H_
+#define _UNIFIEDDRAGEVENT_H_
+
+#include <string>
+
+#include "Event.h"
+
+class UnifiedDragEvent : public Event {
+
+// Attributes
+private:
+    float _dx, _dy, _dz;
+    
+// Methods
+public:
+    UnifiedDragEvent(char *data);
+    UnifiedDragEvent(std::string& name, std::string& desc, char type, int id, 
+            float* location, float dx, float dy, float dz);
+    
+    virtual ~UnifiedDragEvent() {}
+
+    float getdx();
+    float getdy();
+    float getdz();  
+    
+private:
+    virtual char* serializeData(short& lengthOut);
+};
+#endif /*DRAGEVENT_H_*/
