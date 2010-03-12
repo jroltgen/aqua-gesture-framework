b5ab55422b779ccd860df7990b5ac59f080b61e4
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-03-12 17:18:56 +0000

    Adding hand ID gesture for crickets app.

---
+++ aqua/src/gestures/GetHandIDGesture.cpp
@@ -0,0 +1,92 @@
+/**
+ * GetHandIDGesture.cpp
+ *
+ * This class is the foundation for most universal gestures - it provides
+ * the ability to keep track of ups, downs, and moves and keeps track of
+ * the centroid location for all of the points.
+ * 
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <math.h>
+#include <string>
+
+#include "GetHandIDGesture.h"
+#include "../events/HandIDTouchEvent.h"
+
+using namespace std;
+
+/**
+ * Default Constructor
+ */
+GetHandIDGesture::GetHandIDGesture(
+        EventProcessor* publisher, int regionID) : Gesture(publisher,
+        regionID) {
+}
+
+bool GetHandIDGesture::handleEvent(Event* e) {
+	string handString("HandPositionEvent");
+	string unifiedString("UnifiedEvent");
+	
+	if (e->getName().compare(handString) == 0) {
+		handleHandUpdate(e);
+		return false;
+	} else if (e->getName().compare(unifiedString) == 0) {
+		handleUnifiedEvent(e);
+		// WE CONSUME THESE EVENTS.
+		return true;
+	}
+}
+
+/**
+ * Update this hand position's entry in the list.
+ */
+void GetHandIDGesture::handleHandUpdate(Event* e) {	
+
+	_handPositions[e->getID()][0] = e->getX();
+	_handPositions[e->getID()][1] = e->getY();
+	_handPositions[e->getID()][2] = e->getZ();
+
+}
+
+void GetHandIDGesture::handleUnifiedEvent(Event* e) {
+	// Compute the distance to a particular hand using just the 2D coordinates.
+	map<int, float*>::iterator it;
+
+	float minDistance = -1;
+	int handID = -1;
+
+	for (it = _handPositions.begin(); it != _handPositions.end(); it++) {
+		float xDif = e->getX() - (*it).second[0];
+		float yDif = e->getY() - (*it).second[1];
+		float zDif = e->getZ() - (*it).second[2];
+
+		float distance = sqrt(xDif * xDif + yDif * yDif + zDif * zDif);
+
+		if (distance < minDistance || minDistance == -1) {
+			minDistance = distance;
+			handID = (*it).first;
+		}
+	}
+
+	string name("HandIDTouchEvent");
+	string desc("");
+	HandIDTouchEvent event(name, desc, EVENT_TYPE_OTHER, e->getID(), 
+			e->getLocation(), handID);
+
+	publishEvent(&event);
+}
---
+++ aqua/src/gestures/GetHandIDGesture.h
@@ -0,0 +1,79 @@
+/**
+ * GetHandIDGesture.h
+ *
+ * This class provides unified event to hand event associations.  The gesture
+ * will take all events with the name "UnifiedEvent" and associate them with
+ * hand positions which are sent as events with the name "HandPositionEvent".
+ *
+ * It will then create isntances of "HandIDTouchEvent" and send them along.
+ * These events will have additional information contained within, namely
+ * the handID of the touchpoint.
+ *
+ * The HandIDTouchEvent also contains a field which represents the confidence
+ * level - i.e. how sure we are that this touch point should be associated
+ * with this particular hand.
+ * 
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _GETHANDIDGESTURE_H_
+#define _GETHANDIDGESTURE_H_
+
+#include <map>
+#include <vector>
+
+#include "../EventProcessor.h"
+#include "../events/Event.h"
+#include "Gesture.h"
+
+class GetHandIDGesture : public Gesture {
+
+// Attributes
+private:
+    /**
+     * The collection of the known hand positions - this will be used to
+     * determine the hand which is closest to this touch point.  It maps
+     * a particular hand ID to a triple (x,y,x).
+     */
+	std::map<int, float*> _handPositions;
+
+// Methods
+public:
+    GetHandIDGesture(EventProcessor* publisher, int regionID = -1);
+            
+    virtual bool handleEvent(Event* event);
+    
+private:
+    void handleHandUpdate(Event* e);
+	void handleUnifiedEvent(Event* e);
+};
+
+#ifdef _WIN32
+extern "C" {
+    __declspec (dllexport) Gesture* createGesture(EventProcessor* 
+            publisher, int regionID) {
+		return new GetHandIDGesture(publisher, regionID);
+	}
+}
+#else
+extern "C" {
+    Gesture* createGesture(EventProcessor* publisher, int regionID) {
+        return new GetHandIDGesture(publisher, regionID);
+    }
+}
+#endif
+
+#endif /* GETHANDIDGESTURE_H */
