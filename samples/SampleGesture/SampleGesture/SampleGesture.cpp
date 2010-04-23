fe53de45a1ea00dc4eba5390073fb3c8f41959f3
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-04-23 19:29:28 +0000

    Adding Sample Gesture project

---
+++ samples/SampleGesture/SampleGesture/SampleGesture.cpp
@@ -0,0 +1,54 @@
+/**
+ * SampleGesture.cpp
+ * 
+ * Makes locations negative instead of positive and publishee teh resulting event.
+ * This sample project is provided to demonstrate how to write a simple AQUA-G
+ * gesture.
+ * 
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+ 
+#include "SampleGesture.h"
+#include "events/UnifiedEvent.h"
+
+bool SampleGesture::handleEvent(Event* e) {
+    printf("Upon receiving event: %s, %f, %f, %f\n", e->getName().c_str(), e->getX(), e->getY(), e->getZ());
+    invertEvent(e);
+    return false;
+}
+
+void SampleGesture::invertEvent(Event* e) {
+    float inv[3];
+    Event* newEvent;
+
+    std::string desc = "Inverted Event";
+    
+    // Invert x, y and z.
+    printf("Original event: %f, %f, %f\n", e->getX(), e->getY(), e->getZ());
+    inv[0] = 0 - e->getX();
+    inv[1] = 0 - e->getY();
+    inv[2] = 0 - e->getZ();
+    printf("Inverted location: %f, %f, %f\n", inv[0], inv[1], inv[2]);
+
+    //newEvent = new UnifiedEvent(e->getName(), desc, e->getType(), e->getID(), inv);
+
+    //publishEvent(newEvent);
+    //delete(newEvent);
+
+}
---
+++ samples/SampleGesture/SampleGesture/SampleGesture.h
@@ -0,0 +1,58 @@
+/**
+ * HelloWorldGesture.cpp
+ * 
+ * Prints "Hello World" when an event is received.
+ * 
+ * This file is part of Aqua Universal Gesture Recognition Framework.
+ * Copyright (C) 2010  Jay Roltgen.
+ *
+ * Aqua is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _SAMPLEGESTURE_H_
+#define _SAMPLEGESTURE_H_
+
+#include "events/Event.h"
+#include "Gesture.h"  
+
+class SampleGesture : public Gesture {
+
+// Attributes
+
+
+// Methods
+public:
+    SampleGesture(EventProcessor* publisher, int regionID = -1) :
+            Gesture(publisher, regionID) {};
+    virtual bool handleEvent(Event* event);
+    
+private:
+    void invertEvent(Event* e);
+};
+
+#ifdef _WIN32
+extern "C" {
+    __declspec (dllexport) Gesture* createGesture(EventProcessor* 
+            publisher, int regionID) {
+		return new SampleGesture(publisher, regionID);
+	}
+}
+#else
+extern "C" {
+    Gesture* createGesture(EventProcessor* publisher, int regionID) {
+        return new SampleGesture(publisher, regionID);
+    }
+}
+#endif
+
+#endif
