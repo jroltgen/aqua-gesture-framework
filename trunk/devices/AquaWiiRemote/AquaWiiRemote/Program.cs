f6e872cb3678864e0181c7358f65af50ecbca42f
jroltgen <jroltgen@2472bc04-0ab3-11df-a582-91aacf084772>
2010-05-10 21:38:45 +0000

    Updated to handle multiple Wiimotes connected simultaneously.

--- trunk/devices/AquaWiiRemote/AquaWiiRemote/Program.cs
+++ trunk/devices/AquaWiiRemote/AquaWiiRemote/Program.cs
@@ -10,13 +10,14 @@ namespace AquaWiiRemote
 {
     class Program
     {
+        WiimoteCollection wc;
         Wiimote wm = new Wiimote();
         Socket aquaSocket;
-        bool pressed;
-        float lastZoomDistance;
-        float lastAngle;
-
-        bool discard = false;
+        Dictionary<Guid, int> _hashcodetoid;
+        Dictionary<int, bool> pressed;
+        Dictionary<int, float> lastZoomDistances;
+        Dictionary<int, float> lastAngles;
+        Dictionary<int, bool> discard;
 
         static void Main(string[] args)
         {
@@ -26,6 +27,7 @@ namespace AquaWiiRemote
         private void Run()
         {
             // Set up Aqua connection
+            int index = 1;
             byte[] buffer = new byte[1];
             buffer[0] = 2;
             aquaSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -33,12 +35,34 @@ namespace AquaWiiRemote
             aquaSocket.Send(buffer);
 
             // Set up Wiimote
-            wm.WiimoteChanged += WiiMoteChanged;
-            wm.Connect();
-            wm.SetReportType(InputReport.IRAccel, true);
-            
-
-            
+            wc = new WiimoteCollection();
+            try
+            {
+                wc.FindAllWiimotes();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error finding wiimotes.");
+            }
+            Console.WriteLine("Got collection");
+            _hashcodetoid = new Dictionary<Guid, int>();
+            lastAngles = new Dictionary<int, float>();
+            lastZoomDistances = new Dictionary<int, float>();
+            pressed = new Dictionary<int, bool>();
+            discard = new Dictionary<int, bool>();
+            foreach (Wiimote wm in wc)
+            {
+                wm.Connect();
+                wm.SetReportType(InputReport.IRAccel, true);
+                _hashcodetoid.Add(wm.ID, index);
+                Console.WriteLine("Connection: " + _hashcodetoid[wm.ID]);
+                lastAngles.Add(index, -1);
+                lastZoomDistances.Add(index, -1);
+                pressed.Add(index, false);
+                discard.Add(index, false);
+                wm.SetLEDs(index++);
+                wm.WiimoteChanged += WiiMoteChanged;
+            }            
 
             // Keep running
             while (true)
@@ -50,10 +74,9 @@ namespace AquaWiiRemote
         private void WiiMoteChanged(object sender, WiimoteChangedEventArgs args)
         {
             WiimoteState state = args.WiimoteState;
-
             float x = state.IRState.Midpoint.X;
             float y = state.IRState.Midpoint.Y;
-
+            int id = _hashcodetoid[((Wiimote)sender).ID];
             
             
 
@@ -64,15 +87,14 @@ namespace AquaWiiRemote
 
             if (state.ButtonState.B)
             {
-                if (!pressed)
+                if (!pressed[id])
                 {
-                    DoEvent(UnifiedEvent.EVENT_TYPE_DOWN, location);
-                    pressed = true;
+                    DoEvent(UnifiedEvent.EVENT_TYPE_DOWN, location, id);
+                    pressed[id] = true;
                 }
                 else
                 {
-                    discard = !discard;
-                    if (!discard)
+                    if (!discard[id])
                     {
                         // Do move, zoom and rotate if necessary.
                         if (location[0] != 1 && location[1] != 0)
@@ -82,64 +104,65 @@ namespace AquaWiiRemote
                             float newZoomDistance = (float)Math.Sqrt(dx * dx + dy * dy);
                             float newAngle = (float)Math.Atan2(dy, dx);
 
-                            if (lastZoomDistance != -1)
+                            if (lastZoomDistances[id] != -1)
                             {
-                                float scale = lastZoomDistance / newZoomDistance;
-                                scale = (scale - 1) * 4 + 1;
-                                DoZoomEvent(location, scale);
+                                float scale = lastZoomDistances[id] / newZoomDistance;
+                                scale = (scale - 1) * 3 + 1;
+                                DoZoomEvent(location, scale, id);
                             }
 
-                            if (lastAngle != -1)
+                            if (lastAngles[id] != -1)
                             {
-                                float angle = newAngle - lastAngle;
-                                DoRotate(location, angle * 2);
+                                float angle = newAngle - lastAngles[id];
+                                //DoRotate(location, angle, id);
                                 //Console.WriteLine("Angle: " + angle);
                             }
-                            DoEvent(UnifiedEvent.EVENT_TYPE_MOVE, location);
+                            DoEvent(UnifiedEvent.EVENT_TYPE_MOVE, location, id);
 
-                            lastZoomDistance = newZoomDistance;
-                            lastAngle = newAngle;
+                            lastZoomDistances[id] = newZoomDistance;
+                            lastAngles[id] = newAngle;
                         }
                     }
+                    discard[id] = !discard[id];
                 }
             }
             else
             {
-                if (pressed)
+                if (pressed[id])
                 {
-                    DoEvent(UnifiedEvent.EVENT_TYPE_UP, location);
-                    lastZoomDistance = -1;
-                    lastAngle = -1;
-                    pressed = false;
+                    DoEvent(UnifiedEvent.EVENT_TYPE_UP, location, id);
+                    lastZoomDistances[id] = -1;
+                    lastAngles[id] = -1;
+                    pressed[id] = false;
                 }
                 else
                 {
-                    discard = !discard;
-                    if (!discard)
+                    if (!discard[id])
                     {
                         if (location[0] != 1 && location[1] != 0)
-                            DoEvent(UnifiedEvent.EVENT_TYPE_HOVER, location);
+                            DoEvent(UnifiedEvent.EVENT_TYPE_HOVER, location, id);
                     }
+                    discard[id] = !discard[id];
                 }
             }
 
         }
 
-        private void DoZoomEvent(float[] location, float scale)
+        private void DoZoomEvent(float[] location, float scale, int id)
         {
             
             UnifiedZoomEvent e = new UnifiedZoomEvent("UnifiedZoomEvent", "WiiMoteZoom",
-                    (byte)UnifiedEvent.EVENT_TYPE_OTHER, 1, location, scale, location);
+                    (byte)UnifiedEvent.EVENT_TYPE_OTHER, 0, location, scale, location);
             sendEvent(e);
         }
 
-        private void DoRotate(float[] location, float angle)
+        private void DoRotate(float[] location, float angle, int id)
         {
 
             Unified2DRotateEvent e = new Unified2DRotateEvent("Unified2DRotateEvent", "WiiMoteRotate",
-                    (byte)UnifiedEvent.EVENT_TYPE_OTHER, 1, location, angle, location);
-            sendEvent(e);
-
+                    (byte)UnifiedEvent.EVENT_TYPE_OTHER, 0, location, angle, location);
+            //sendEvent(e);
+            Console.WriteLine(angle);
             short length;
             byte[] bytesToSend = e.serialize(out length);
             //Console.Write("Len " + length + ", Data: ");
@@ -150,10 +173,10 @@ namespace AquaWiiRemote
             //Console.WriteLine();
         }
 
-        private void DoEvent(int type, float[] location)
+        private void DoEvent(int type, float[] location, int id)
         {
             UnifiedEvent e = new UnifiedEvent("UnifiedEvent", "WiiMoteTouch",
-                    (byte)type, 1, location);
+                    (byte)type, id, location);
             sendEvent(e);
             //Console.WriteLine("Type: " + type + ", loc: " + location[0] + ", " + location[1] + ", " + location[2]);
         }
